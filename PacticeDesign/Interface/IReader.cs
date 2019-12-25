4a7cba7f1d4605bcbbe20d037b7d451d9e1e5771
Rajib <r.l.panigrahi4@gmail.com>
2019-12-25 20:04:17 +0530

    Checkin

---
+++ PacticeDesign/Interface/IReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacticeDesign.Interface
+{
+    /// <summary>
+    /// Read the json file
+    /// </summary>
+    interface IReader
+    {
+        void ReadJson();
+
+    }
+}
